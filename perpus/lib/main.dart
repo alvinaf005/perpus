d2ab23f697d89ae9ab892cbb7446c0121fdabdbe
alvinaf005 <alvinaratupranta@gmail.com>
2022-11-10 22:01:43 +0700

    alvina

---
+++ perpus/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:perpus/dataPengguna/MasukPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Library Books',
+
+      /// TODO Replace with your first screen class name
+      home: MasukPage(),
+    );
+  }
+}
